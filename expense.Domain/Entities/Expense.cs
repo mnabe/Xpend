a7fd5cd42ed22247c0d4d4c481fc335f277e8136
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-25 15:47:31 +0200

    Add domain entity expense

---
+++ expense.Domain/Entities/Expense.cs
@@ -0,0 +1,16 @@
+﻿using expense.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expense.Domain.Entities
+{
+    public class Expense
+    {
+        public int ExpenseId { get; set; }
+        public string ExpenseReason { get; set; }
+        public decimal ExpenseCost { get; set; }
+        public ExpenseStatus ExpenseStatus { get; set; }
+
+    }
+}
---
+++ expense.Domain/Enums/ExpenseStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expense.Domain.Enums
+{
+    public enum ExpenseStatus
+    {
+        RECEIVED = 1,
+        ACCEPTED = 2,
+        DECLINED = 3
+    }
+}
