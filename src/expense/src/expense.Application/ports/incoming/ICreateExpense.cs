7dfb180318cb51f62f347b4ca3eeb59590ce2063
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-26 05:37:03 +0200

    Add incoming port/interface

---
+++ src/expense/src/expense.Application/ports/incoming/ICreateExpense.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expense.Application.ports.incoming
+{
+    public interface ICreateExpense
+    {
+        public bool CreateExpense (CreateExpenseCommand command)
+    }
+}
