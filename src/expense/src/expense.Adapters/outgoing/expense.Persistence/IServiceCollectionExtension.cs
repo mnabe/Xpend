0ff814e1fc114201dc081cce7b85f71f3a2ea584
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-26 08:15:26 +0200

    Add extension for iservicecollection to persistence library

---
+++ src/expense/src/expense.Adapters/outgoing/expense.Persistence/IServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expense.Persistence
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddPersistenceDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ExpenseDataContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ExpenseConnection")));
+            return services;
+        }
+    }
+}
