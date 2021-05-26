c93ff4079741fa12643aa82693d98fd784eead83
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-26 05:41:52 +0200

    Service implements port

--- src/expense/src/expense.Application/services/CreateExpenseService.cs
+++ src/expense/src/expense.Application/services/CreateExpenseService.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using expense.Application.ports.incoming;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace expense.Application.services
 {
-    class CreateExpenseService
+    class CreateExpenseService : ICreateExpense
     {
-
+        public bool CreateExpense(CreateExpenseCommand command)
+        {
+            
+        }
     }
 }
