21bb642b8cdcc41c63297c316aaf3d431a6255fe
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-26 07:39:47 +0200

    Add controller test

--- src/expense/test/ExpenseTests/Adapters/incoming/ExpenseControllerTests.cs
+++ src/expense/test/ExpenseTests/Adapters/incoming/ExpenseControllerTests.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using expense.WebApi.Controllers;
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using Xunit;
 
 namespace ExpenseTests.Adapters.incoming
 {
-    class ExpenseControllerTests
+    public class ExpenseControllerTests
     {
+        [Fact]
+        public void CreateExpense_Success()
+        {
+            //Arrange
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var expenseController = fixture.Build<ExpenseController>().OmitAutoProperties().Create();
+            string expenseCategory = "HOTEL";
+            decimal expenseCost = 700;
+
+            //Act
+            HttpStatusCode result = (HttpStatusCode)expenseController.CreateExpense(expenseCategory, expenseCost).StatusCode.Value;
+
+            //Assert
+            Assert.True(HttpStatusCode.OK.Equals(result));
+        }
+
+        //[Fact]
+        //public void CreateExpense_ThrowsException()
     }
 }
