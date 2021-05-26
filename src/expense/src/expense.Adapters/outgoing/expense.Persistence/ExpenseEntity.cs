096538abeceb0c51888194bf05875481137f92d7
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-26 08:11:16 +0200

    Add db entity expense

---
+++ src/expense/src/expense.Adapters/outgoing/expense.Persistence/ExpenseEntity.cs
@@ -0,0 +1,15 @@
+﻿using expense.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expense.Persistence
+{
+    public class ExpenseEntity
+    {
+        public int ExpenseId { get; set; }
+        public ExpenseCategory ExpenseCategory { get; set; }
+        public decimal ExpenseCost { get; set; }
+        public ExpenseStatus ExpenseStatus { get; set; }
+    }
+}
