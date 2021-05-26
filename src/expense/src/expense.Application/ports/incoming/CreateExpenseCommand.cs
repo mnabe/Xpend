db67cd416c6774fd77cae1752b9abb25d4f09ab7
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-26 05:41:10 +0200

    Create expense command

---
+++ src/expense/src/expense.Application/ports/incoming/CreateExpenseCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expense.Application.ports.incoming
+{
+    public class CreateExpenseCommand
+    {
+        public string ExpenseCategory { get; set; }
+        public decimal ExpenseCost { get; set; }
+        public CreateExpenseCommand(string expenseCategory, decimal expenseCost)
+        {
+            ExpenseCategory = expenseCategory;
+            ExpenseCost = expenseCost;
+        }
+    }
+}
--- src/expense/test/ExpenseTests/Domain/ExpenseTests.cs
+++ src/expense/test/ExpenseTests/Domain/ExpenseTests.cs
@@ -24,7 +24,6 @@ namespace ExpenseTests.Domain
             //Assert
             Assert.True(true);
         }
-
         //[Fact]
         //public void CreatesExpense()
 
