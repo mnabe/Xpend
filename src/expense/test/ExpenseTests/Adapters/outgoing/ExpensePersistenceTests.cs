96804d03db54f7e8e66b3a5efc1f38be5a896e98
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-27 21:18:45 +0200

    Add persistence test using the dbfixture

---
+++ src/expense/test/ExpenseTests/Adapters/outgoing/ExpensePersistenceTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoFixture;
+using expense.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+using expense.Persistence;
+
+namespace ExpenseTests.Adapters.outgoing
+{
+    public class ExpensePersistenceTests: IClassFixture<DbFixture>
+    {
+        DbFixture _DbFixture;
+        public ExpensePersistenceTests(DbFixture dbFixture)
+        {
+            _DbFixture = dbFixture;
+        }
+
+        [Fact]
+        public void ExpenseAddedToDb_Success()
+        {
+            //Arrange
+            Fixture fixture = new Fixture();
+            ExpenseEntity expense = fixture.Create<ExpenseEntity>();
+
+            //Act
+            _DbFixture.ExpenseDataContext.Expenses.Add(expense);
+            _DbFixture.ExpenseDataContext.SaveChanges();
+
+            //Assert
+            Assert.Equal(1, _DbFixture.ExpenseDataContext.Expenses.Count());
+        }
+    }
+}
