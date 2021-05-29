23de391b13b2c3ade023c18b9fa339c49a4a3dfc
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-29 22:15:10 +0200

    Add service for getting expenses

---
+++ src/expense/src/expense.Application/services/GetExpensesService.cs
@@ -0,0 +1,23 @@
+﻿using expense.Application.ports.incoming;
+using expense.Application.ports.outgoing;
+using expense.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expense.Application.services
+{
+    public class GetExpensesService : IGetExpenses
+    {
+        private readonly IFindExpenses _findExpenses;
+        public GetExpensesService(IFindExpenses findExpenses)
+        {
+            _findExpenses = findExpenses;
+        }
+        public List<Expense> GetExpenses()
+        {
+            List<Expense> expenses = _findExpenses.Find();
+            return expenses;
+        }
+    }
+}
