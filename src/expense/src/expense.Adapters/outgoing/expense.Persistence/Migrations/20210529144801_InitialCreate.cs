f02dbff8aea12cf733680e4ec9317c9304da54fc
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-29 17:00:33 +0200

    Add initial migration

---
+++ src/expense/src/expense.Adapters/outgoing/expense.Persistence/Migrations/20210529144801_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace expense.Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    ExpenseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExpenseCategory = table.Column<int>(type: "int", nullable: false),
+                    ExpenseCost = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ExpenseStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.ExpenseId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expenses");
+        }
+    }
+}
