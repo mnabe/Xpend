5f13cdf6976b467ebdda872e57ec86d68474422a
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-29 23:34:35 +0200

    Add service for single expense

---
+++ src/expense/src/expense.Application/services/GetExpenseService.cs
@@ -0,0 +1,23 @@
+﻿using expense.Application.ports.incoming;
+using expense.Application.ports.outgoing;
+using expense.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expense.Application.services
+{
+    public class GetExpenseService: IGetExpense
+    {
+        private readonly IFindExpense _findExpense;
+        public GetExpenseService(IFindExpense findExpense)
+        {
+            _findExpense = findExpense;
+        }
+        public Expense GetExpense(int id)
+        {
+            Expense expense = _findExpense.Find(id);
+            return expense;
+        }
+    }
+}
