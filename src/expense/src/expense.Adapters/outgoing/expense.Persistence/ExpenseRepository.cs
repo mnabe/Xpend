4327f761b3bad3cbea8243b7fd66a28810228993
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-29 23:34:02 +0200

    Add repository method for single expense

--- src/expense/src/expense.Adapters/outgoing/expense.Persistence/ExpenseRepository.cs
+++ src/expense/src/expense.Adapters/outgoing/expense.Persistence/ExpenseRepository.cs
@@ -9,7 +9,7 @@ using System.Linq;
 
 namespace expense.Persistence
 {
-    public class ExpenseRepository: IAddExpense, IFindExpenses
+    public class ExpenseRepository: IAddExpense, IFindExpenses, IFindExpense
     {
         private readonly IMapper _mapper;
         private readonly ExpenseDataContext _context;
@@ -19,7 +19,18 @@ namespace expense.Persistence
             _mapper = mapper;
             _context = context;
         }
-
+        public Expense Find(int id)
+        {
+            var response = _context.Expenses.Find(id);
+            Expense expense = _mapper.Map<Expense>(response);
+            return expense;
+        }
+        public List<Expense> Find()
+        {
+            var response = _context.Expenses.AsEnumerable();
+            List<Expense> expenses = _mapper.Map<List<Expense>>(response);
+            return expenses;
+        }
         public Expense Add(ExpenseCategory expenseCategory, decimal expenseCost)
         {
             Expense expense = new Expense();
@@ -29,13 +40,6 @@ namespace expense.Persistence
             _context.Expenses.Add(entity);
             _context.SaveChanges();
             return expense;
-        }
-
-        public List<Expense> Find()
-        {
-            var response = _context.Expenses.AsEnumerable();
-            List<Expense> expenses = _mapper.Map<List<Expense>>(response);
-            return expenses;
         }
     }
 }
