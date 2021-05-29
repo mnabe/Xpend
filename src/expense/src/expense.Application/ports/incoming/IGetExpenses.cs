360df20b0d56383b774292b01f6c9021eb557fe7
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-29 22:14:58 +0200

    Add ports for getting expenses

---
+++ src/expense/src/expense.Application/ports/incoming/IGetExpenses.cs
@@ -0,0 +1,12 @@
+﻿using expense.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expense.Application.ports.incoming
+{
+    public interface IGetExpenses
+    {
+        public List<Expense> GetExpenses();
+    }
+}
---
+++ src/expense/src/expense.Application/ports/outgoing/IFindExpenses.cs
@@ -0,0 +1,12 @@
+﻿using expense.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expense.Application.ports.outgoing
+{
+    public interface IFindExpenses
+    {
+        List<Expense> Find();
+    }
+}
