cb666606baab414556bda41a5b8b587f07599eb8
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-30 00:23:35 +0200

    Create shared expensecategoryservice class

---
+++ src/expense/src/expense.Application/services/Shared/ExpenseCategoryService.cs
@@ -0,0 +1,16 @@
+﻿using expense.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expense.Application.services.Shared
+{
+    public class ExpenseCategoryService
+    {
+        public ExpenseCategory checkValidCategoryEnum(string expenseCategoryString)
+        {
+            ExpenseCategory expenseCategory = (ExpenseCategory)Enum.Parse(typeof(ExpenseCategory), expenseCategoryString);
+            return expenseCategory;
+        }
+    }
+}
