7bd5eef700687909d5488f5482245e111a611499
mnabe <32497085+mnabe@users.noreply.github.com>
2021-05-31 15:13:51 +0200

    Rename to testfixture

---
+++ src/expense/test/ExpenseTests/TestFixture.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using expense.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExpenseTests
+{
+    public class TestFixture: IDisposable
+    {
+        private readonly DbContextOptions<ExpenseDataContext> _options;
+        public ExpenseDataContext ExpenseDataContext { get; private set; }
+        public Fixture Fixture { get; private set; }
+        public TestFixture()
+        {
+            _options = new DbContextOptionsBuilder<ExpenseDataContext>()
+               .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            ExpenseDataContext = new ExpenseDataContext(_options);
+            Fixture = new Fixture();
+        }
+
+        public void Dispose()
+        {
+            ExpenseDataContext.Dispose();
+
+        }
+    }
+}
