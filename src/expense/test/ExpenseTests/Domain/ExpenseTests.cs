d7ee652dc86385af0745af0a01dafc34872302e2
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-01 17:32:28 +0200

    Remove unused imports from domain tests

--- src/expense/test/ExpenseTests/Domain/ExpenseTests.cs
+++ src/expense/test/ExpenseTests/Domain/ExpenseTests.cs
@@ -1,9 +1,6 @@
 ﻿using AutoFixture;
 using expense.Domain.Entities;
 using expense.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace ExpenseTests.Domain
