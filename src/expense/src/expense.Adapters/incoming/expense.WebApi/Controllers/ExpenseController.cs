11e66f25b487fe3c42c0b4cf0841735a8fa42a8f
mnabe <32497085+mnabe@users.noreply.github.com>
2021-06-01 17:22:13 +0200

    Remove unused imports from webapi project

--- src/expense/src/expense.Adapters/incoming/expense.WebApi/Controllers/ExpenseController.cs
+++ src/expense/src/expense.Adapters/incoming/expense.WebApi/Controllers/ExpenseController.cs
@@ -1,10 +1,5 @@
 ﻿using expense.Application.ports.incoming;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace expense.WebApi.Controllers
 {
--- src/expense/src/expense.Adapters/incoming/expense.WebApi/Startup.cs
+++ src/expense/src/expense.Adapters/incoming/expense.WebApi/Startup.cs
@@ -6,16 +6,9 @@ using expense.Domain.Entities;
 using expense.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace expense.WebApi
 {
