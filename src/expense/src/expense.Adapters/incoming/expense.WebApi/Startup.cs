c296c4374aa70f7fc90c54a3537417ab0de66520
mnabe <32497085+mnabe@users.noreply.github.com>
2021-07-22 10:21:21 +0200

    Remove unused import

--- src/expense/src/expense.Adapters/incoming/expense.WebApi/Startup.cs
+++ src/expense/src/expense.Adapters/incoming/expense.WebApi/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Sciensoft.Hateoas;
 using Sciensoft.Hateoas.Extensions;
 using System.Reflection;
 
