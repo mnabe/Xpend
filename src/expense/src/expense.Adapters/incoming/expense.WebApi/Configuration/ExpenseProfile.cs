0eb211a4cab66d392c4ad4229a0bf81c7d25a863
mnabe <32497085+mnabe@users.noreply.github.com>
2021-07-22 10:25:23 +0200

    Create AutoMapper profile for mappings

---
+++ src/expense/src/expense.Adapters/incoming/expense.WebApi/Configuration/ExpenseProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using expense.Domain.Entities;
+using expense.Persistence;
+
+namespace expense.WebApi.Configuration
+{
+    public class ExpenseProfile: Profile
+    {
+        public ExpenseProfile()
+        {
+            CreateMap<Expense, ExpenseEntity>();
+            CreateMap<ExpenseEntity, Expense>();
+        }
+    }
+}
