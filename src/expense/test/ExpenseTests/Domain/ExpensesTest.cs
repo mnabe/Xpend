a9ed927bcca0f2f5ae28fb9ad83ae0c0c5bd3994
mnabe <32497085+mnabe@users.noreply.github.com>
2021-07-29 12:27:26 +0200

    Add simple domain test

---
+++ src/expense/test/ExpenseTests/Domain/ExpensesTest.cs
@@ -0,0 +1,19 @@
+﻿using expense.Domain.Entities;
+using expense.Domain.Enums;
+using Xunit;
+
+namespace ExpenseTests.Domain
+{
+    public class ExpensesTest
+    {
+        [Fact]
+        public void CreateExpense_Success()
+        {
+            //Act
+            Expense expense = new Expense(ExpenseCategory.HOTEL, 200);
+
+            //Assert
+            Assert.Equal("HOTEL", expense.ExpenseCategory.ToString());
+        }
+    }
+}
