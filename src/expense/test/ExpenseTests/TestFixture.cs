5510a24c5a2f4dc315c6dd66cd918d386ef34d45
mnabe <32497085+mnabe@users.noreply.github.com>
2021-07-30 20:48:56 +0200

    Remove explicit dbFixture class for now and re-add logic in TestFixture

--- src/expense/test/ExpenseTests/TestFixture.cs
+++ src/expense/test/ExpenseTests/TestFixture.cs
@@ -1,18 +1,26 @@
 ﻿using AutoFixture;
 using AutoFixture.AutoMoq;
+using expense.Domain.Enums;
 using expense.Persistence;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 
 namespace ExpenseTests
 {
     public class TestFixture
     {
+        private readonly DbContextOptions<ExpenseDataContext> _options;
+        internal ExpenseDataContext ExpenseDataContext { get; private set; }
         public Fixture Fixture { get; private set; }
         public TestFixture()
         {
+            _options = new DbContextOptionsBuilder<ExpenseDataContext>()
+               .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            ExpenseDataContext = new ExpenseDataContext(_options);
+
             Fixture = new Fixture();
-            Fixture.Customize(new AutoMoqCustomization());
         }
     }
 }
