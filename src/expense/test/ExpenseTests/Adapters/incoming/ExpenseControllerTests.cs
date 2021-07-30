11d7b6460beeba904c4a8b7b28b3a6aadad22915
mnabe <32497085+mnabe@users.noreply.github.com>
2021-07-30 20:51:37 +0200

    Make GetExpense controllertest pass with Moq

--- src/expense/test/ExpenseTests/Adapters/incoming/ExpenseControllerTests.cs
+++ src/expense/test/ExpenseTests/Adapters/incoming/ExpenseControllerTests.cs
@@ -1,8 +1,11 @@
 ﻿using AutoFixture;
 using AutoFixture.AutoMoq;
+using expense.Application.ports.incoming;
+using expense.Domain.Entities;
 using expense.WebApi.Controllers;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System.Net;
 using System.Threading.Tasks;
 using Xunit;
@@ -11,13 +14,20 @@ namespace ExpenseTests.Adapters.incoming
 {
     public class ExpenseControllerTests
     {
+        private TestFixture _fixture;
+        public ExpenseControllerTests()
+        {
+            _fixture = new TestFixture();
+            _fixture.Fixture.Customize(new AutoMoqCustomization());
+        }
         [Fact]
         public async Task GetExpense_Success()
         {
-            //Setup
-            TestFixture _testFixture = new TestFixture();
-            _testFixture.Fixture.Customize(new AutoMoqCustomization());
-            var expenseController = _testFixture.Fixture.Build<ExpenseController>().OmitAutoProperties().Create();
+            //Arrange
+            var service = _fixture.Fixture.Freeze<Mock<IGetExpense>>();
+            var expense = _fixture.Fixture.Build<Expense>().With(c => c.ExpenseId, 1).Create();
+            service.Setup(a => a.GetExpense(1)).ReturnsAsync(expense);
+            var expenseController = _fixture.Fixture.Build<ExpenseController>().OmitAutoProperties().Create();
 
             //Act
             var result = await expenseController.GetExpense(1);
@@ -25,41 +35,36 @@ namespace ExpenseTests.Adapters.incoming
             //Assert
             ObjectResult objectResult = Assert.IsType<OkObjectResult>(result);
             Assert.Equal(200, objectResult.StatusCode);
-            result.Should().BeOfType<Task<IActionResult>>();
+            //result.Should().BeOfType<Task<IActionResult>>();
         }
         [Fact]
-        public void GetExpenses_Success()
+        public async Task GetExpenses_Success()
         {
             //Arrange
-            TestFixture _testFixture = new TestFixture();
-            _testFixture.Fixture.Customize(new AutoMoqCustomization());
-            var expenseController = _testFixture.Fixture.Build<ExpenseController>().OmitAutoProperties().Create();
+            var expenseController = _fixture.Fixture.Build<ExpenseController>().OmitAutoProperties().Create();
 
             //Act
-            var result = expenseController.GetExpenses();
+            var result = await expenseController.GetExpenses();
 
             //Assert
-            result.Should().BeOfType<OkObjectResult>()
-                .Which.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            ObjectResult objectResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, objectResult.StatusCode);
+            result.Should().BeOfType<Task<IActionResult>>();
         }
 
         [Fact]
-        public void CreateExpense_Success()
+        public async Task CreateExpense_Success()
         {
             //Arrange
-            TestFixture _testFixture = new TestFixture();
-            _testFixture.Fixture.Customize(new AutoMoqCustomization());
-            var expenseController = _testFixture.Fixture.Build<ExpenseController>().OmitAutoProperties().Create();
+            var expenseController = _fixture.Fixture.Build<ExpenseController>().OmitAutoProperties().Create();
             string expenseCategory = "HOTEL";
             decimal expenseCost = 700;
 
             //Act
-
-            var result = expenseController.CreateExpense(expenseCategory, expenseCost);
+            var result = await expenseController.CreateExpense(expenseCategory, expenseCost);
 
             //Assert
-            result.Should().BeOfType<CreatedAtActionResult>()
-                .Which.StatusCode.Should().Be((int)HttpStatusCode.Created);
+            result.Should().BeOfType<CreatedAtActionResult>();
         }
 
         [Fact]
